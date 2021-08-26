f61f1c6e31e44c02ba9749c2a34e8fdb38aca31b
Sarah Drew <drew.sarah.e@gmail.com>
2021-08-25 17:44:01 -0700

    fixed padding, moved 2nd date to the right

--- src/components/ReviewsPage/appendButton.js
+++ src/components/ReviewsPage/appendButton.js
@@ -2,16 +2,11 @@ export default function appendButton(){
   let reviewTextContainer = document.getElementsByClassName("review-text-container");
   let buttonContainer = document.getElementsByClassName("button-container");
   
-  // console.log(reviewTextContainer[1]);
-  // console.log(buttonContainer[1]);
-  
   const mediaQuery = window.matchMedia('(min-width: 769px)');
   
   function appendToContainer(){
     if (mediaQuery.matches) {
       for (let i = 0; i < reviewTextContainer.length; i++){
-        console.log(reviewTextContainer[i]);
-        console.log(buttonContainer[i]);
         reviewTextContainer[i].appendChild(buttonContainer[i]);
       }
     }
